92e4fec10ad1fa2e912a7aa8201452939855e84e
petersudai <psudai@gmail.com>
2024-03-04 22:25:32 -0800

    Sibling

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -13,7 +13,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 		return (NULL);
 
 	if (node->parent->left == node)
-		return (node->parent->);
+		return (node->parent->right);
 	else if (node->parent->right == node)
 		return (node->parent->left);
 	else
