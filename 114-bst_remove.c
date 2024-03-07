9aa93a4dacd9ccd08d32f45e7b8c907e6e8d27b6
petersudai <psudai@gmail.com>
2024-03-07 02:33:01 -0800

    BST - Remove

---
+++ 114-bst_remove.c
@@ -0,0 +1,43 @@
+#include "binary_trees.h"
+
+/**
+ * bst_remove - Removes a node from a Binary Search Tree
+ * @root: Pointer to the root node of the tree where you will remove a node
+ * @value: The value to remove in the tree
+ *
+ * Return: A pointer to the new root node of tree after removing the desired value
+ */
+
+bst_t *bst_rremove(bst_t *root, int value)
+{
+	if (root == NULL)
+		return (NULL);
+
+	if (value < root->n)
+		root->left = bst_remove(root->left, value);
+	else if (value > root->n)
+		root->right = bst_remove(root->right, value);
+	else
+	{
+		if (root->left == NULL)
+		{
+			bst_t *right_child = root->right;
+			free(root);
+			return (right_child);
+		}
+		else if (root->right == NULL)
+		{
+			bst_t *left_child = root->left;
+			free(root);
+			return (left_child);
+		}
+
+		bst_t *temp = root->right;
+		while (temp->left != NULL)
+			temp = temp->left;
+
+		root->n = temp->n;
+		root->right = bst_remove(root->right, temp->n);
+	}
+	return (root);
+}
