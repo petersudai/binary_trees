5d8fc7134072374532a43bd48446f91bb2f501e4
petersudai <psudai@gmail.com>
2024-03-07 20:46:44 -0800

    AVL - Array to AVL

--- 122-array_to_avl.c
+++ 122-array_to_avl.c
@@ -21,7 +21,7 @@ avl_t *array_to_avl(int *array, size_t size)
 
 	for (i = 0; i < size; i++)
 	{
-		if (binary_tree_search(root, array[i]) == NULL)
+		if (bst_search(root, array[i]) == NULL)
 		{
 			if (avl_insert(&root, array[i]) == NULL)
 				return (NULL);
